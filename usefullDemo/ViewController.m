006220d51312aa9495524f55c517182b858372dc
Yorke <zc_and_zc@aliyun.com>
2014-11-07 22:58:25 +0800

    init

---
+++ usefullDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  usefullDemo
+//
+//  Created by Yorke on 14/11/7.
+//  Copyright (c) 2014年 Yorke. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NSDictionary+Safe.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
